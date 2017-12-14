35f8475617a4f0f4667d294002bc2ea398b35fca
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-14 13:11:21 -0500

    12/14/17

--- Lab 4.1/src/Deck.java
+++ Lab 4.1/src/Deck.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
 
 public class Deck {
+	private ArrayList<Card> unDealt;
+	private ArrayList<Card> Dealt;
+	
+	public Deck(String[] rank, String[] suit, int[] pointValue) 
+	{
+		
+	}
+	
+	public isEmpty()
+	{
 
-	public Deck() {
-		// TODO Auto-generated constructor stub
 	}
 
+	public size()
+	{
+		
+	}
+	
+	public deal()
+	{
+		
+	}
+	
+	public shuffle()
+	{
+		
+	}
 }
