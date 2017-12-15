6d1c02c847518eb4c69665883421799d94193ff4
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-15 13:14:42 -0500

    12/15/17

--- Lab 4.1/src/Deck.java
+++ Lab 4.1/src/Deck.java
@@ -6,26 +6,48 @@ public class Deck {
 	
 	public Deck(String[] rank, String[] suit, int[] pointValue) 
 	{
-		
+		for (int i = 0; i < rank.length; i++)
+		{
+			for (int j = 0; j < suit.length; j++)
+			{
+				Card newCard = new Card(rank[i], suit[j], pointValue[i]);
+				unDealt.add(newCard);
+			}
+		}
 	}
 	
-	public isEmpty()
+	public boolean isEmpty()
 	{
+		if(unDealt.size() == 0)
+		{
+			return true;
+		}
+		return false;
+	}
 
-	}
-
-	public size()
+	public ArrayList<Card> size()
 	{
-		
+		return unDealt;
 	}
 	
-	public deal()
+	public Card deal()
 	{
-		
+		Card aCard = unDealt.get(0);
+		if(unDealt.size() == 0)
+		{
+			return null;
+		}
+		else
+		{
+			Dealt.add(unDealt.remove(0));
+			unDealt.remove(0);
+		}
+		return aCard;
 	}
 	
-	public shuffle()
+	public void shuffle()
 	{
-		
+		int r = (int)(Math.random() * 51) + 1;
+		for(int k = )
 	}
 }
