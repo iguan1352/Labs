765a2c6df2d06a20d24c4f741be8ae7eda62e870
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-19 12:50:26 -0500

    12/19/17

---
+++ Lab 4.2 CSV Utilities/src/CSVUtilities.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVUtilities {
+	private File csv;
+	private int numColumns;
+	private ArrayList<String> CSVData;
+	
+	public CSVUtilities(File csv) 
+	{
+		this.csv = csv;
+		this.numColumns = CSVData.size();
+	}
+	
+	public List<String> getColumnHeaders()
+	{
+		return 
+	}
+
+	public List<String> getData(int column)
+	{
+		
+	}
+	
+	public List<Integer> getData(int column)
+	{
+		
+	}
+	
+	public List<Double> getData(int column)
+	{
+		
+	}
+}
