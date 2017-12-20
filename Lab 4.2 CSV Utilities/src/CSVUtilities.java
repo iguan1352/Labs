5c40c13ec4e7b697af9844b3f9899b6b9b9cb61d
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-20 13:02:07 -0500

    12/20/17

--- Lab 4.2 CSV Utilities/src/CSVUtilities.java
+++ Lab 4.2 CSV Utilities/src/CSVUtilities.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +14,24 @@ public class CSVUtilities {
 	private int numColumns;
 	private ArrayList<String> CSVData;
 	
-	public CSVUtilities(File csv) 
+	public CSVUtilities(File csv) throws IOException
 	{
-		this.csv = csv;
 		this.numColumns = 7;
+		Path filePath = Paths.get(csv.getAbsolutePath());
+		BufferedReader file = new BufferedReader(new FileReader(csv));
+		String line = file.readLine();
+		String[] attributes = null;
+		while(line != null)
+		{
+			attributes = line.split(",");
+			
+		}
+		
+		for(String i : attributes)
+		{
+			//if()
+		}
+		
 	}
 	
 	public List<String> getColumnHeaders()
@@ -18,14 +39,15 @@ public class CSVUtilities {
 		List<String> headers = new ArrayList<String>();
 		for(int i = 0; i < numColumns; i++)
 		{
-			headers.add(CSVData[i]);
+			headers.add(CSVData.get(i));
 		}
 		return headers;
 	}
 
 	public List<String> getData(int column)
 	{
-		
+		List<String> columnData = new ArrayList<String>();
+		for(int i = column-1; i < CSVData.size(); i + 7)
 	}
 	
 	public List<Integer> getData(int column)
