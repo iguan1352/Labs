dbdd3ec8f96414f157f431f325c2a7a3d9366a5f
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-02 13:13:46 -0500

    1/2/18

--- Lab 4.3 Button Games/src/GameCode.java
+++ Lab 4.3 Button Games/src/GameCode.java
@@ -1,17 +1,30 @@
 //Ivy Guan
 //Created on January 2, 2018
 
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class GameCode extends Application{
 	
-	public void start(Stage primaryStage) throws Exception
-	{
-		
-	}
 	public static void main(String[] args)
 	{
 		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		primaryStage.setTitle("Fast Clicker"); 
+		
+		Button button = new Button("Click Me!");
+		Scene display = new Scene(button, 100, 100); //the format of stage
+		primaryStage.setScene(display); //sets the format of stage
+        primaryStage.show(); //displays the stage
+        
+        button.setStyle("-fx-font-size: 2");
 	}
 
 
