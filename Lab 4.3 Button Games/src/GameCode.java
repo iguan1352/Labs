807e252bc382a18fdc440f7a3c1f9a8144dc6d58
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-03 13:11:35 -0500

    1/2/18

--- Lab 4.3 Button Games/src/GameCode.java
+++ Lab 4.3 Button Games/src/GameCode.java
@@ -2,16 +2,23 @@
 //Created on January 2, 2018
 
 
+
+import java.beans.EventHandler;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class GameCode extends Application{
 	
 	public static void main(String[] args)
-	{
+	{	
+		//long startTime = System.nanoTime();
 		Application.launch(args);
+		//long 
 	}
 	
 	@Override
@@ -20,11 +27,27 @@ public class GameCode extends Application{
 		primaryStage.setTitle("Fast Clicker"); 
 		
 		Button button = new Button("Click Me!");
-		Scene display = new Scene(button, 100, 100); //the format of stage
-		primaryStage.setScene(display); //sets the format of stage
+		HBox hbox = new HBox(button);
+		Scene display = new Scene(hbox,200,200); //the format of stage
+		button.setMaxSize(80, 30); //sets button size
+		//button.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		button.setOnAction(new EventHandler<ActionEvent>() 
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				if(//if mouse is clicked add one to score)
+				{
+					
+				}
+			}
+		});
+
+		primaryStage.setScene(display); //creates stage using the format
         primaryStage.show(); //displays the stage
         
-        button.setStyle("-fx-font-size: 2");
+
 	}
 
 
